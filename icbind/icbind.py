b507751770e1257f37d3e3168227d96b7d362aac
Zach Anderson <zach.inbox@gmail.com>
2020-08-01 01:11:37 -0500

    Resolve default dockerfile correctly

--- icbind/icbind.py
+++ icbind/icbind.py
@@ -9,21 +9,26 @@ from icbind import directory_sync
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', dest='file', default='./Dockerfile')
+    parser.add_argument('-f', dest='file', default=None)
     parser.add_argument('-d', dest='build_dir', default=None)
     parser.add_argument('PATH')
     parser.add_argument('DOCKER OPTIONS', nargs=argparse.REMAINDER)
 
     args = parser.parse_args(sys.argv[1:])
 
+    # If no custom build directory, generate temporary file
     if not args.build_dir:
         args.build_dir = tempfile.mkdtemp()
+
+    # If no file argument, assume dockerfile is in root of build context
+    if not args.file:
+        args.file = args.PATH + '/Dockerfile'
 
     print("Synchronizing default context to build directory")
     directory_sync(args.PATH, args.build_dir)
 
     # Search the dockerfile for any macros and execute them
-    with open(args.PATH + '/' + args.file) as dockerfile:
+    with open(args.file) as dockerfile:
         for line in dockerfile.readlines():
             for regex, parser in regexes:
                 m = regex.match(line)
